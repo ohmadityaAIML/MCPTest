1f99a059dc640776319332324ec43d5fb12bd305
ohmad <ohmaditya1526@gmail.com>
2026-03-11 20:29:32 +0530

    FirstCommit

---
+++ DataTypesInPython/Data_Typle.py
@@ -0,0 +1,12 @@
+s = "Hello Ohm"
+s1 = "Welcome to Python"
+s2 = s + s1  # Concatenation
+
+print(s)
+print(len(s))
+print(s[0])
+print("****************")
+print(s[0:2])
+print(s[:-1])
+print("********************************")
+print(s[::-1])
